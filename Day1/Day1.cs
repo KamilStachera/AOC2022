b50e204c7f4852738b853e2004d8dbf5c158987a
kamil stachera <kamilstachera5@wp.pl>
2022-12-04 14:50:04 +0100

    Removed unnecessary things from day1

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -17,25 +17,6 @@ namespace AOC.NewFolder
                                      .Select(z => Convert.ToInt32(z)).Sum())
                        .Max();
         }
-
-        public static int FirstTaskNormalWay()
-        {
-            var input = File.ReadAllText(@"..\..\..\Day1\input1.txt");
-            var inputSplitted = input.Split("\r\n\r\n");
-            var max = 0;
-
-            foreach (var values in inputSplitted)
-            {
-                var specificSplitted = values.Split("\r\n");
-                var value = specificSplitted.Select(x => Convert.ToInt32(x)).Sum();
-
-                if (value > max)
-                    max = value;
-            }
-
-            return max;
-        }
-
         public static int SecondTask()
         {
             return File.ReadAllText(@"..\..\..\Day1\input1.txt")
@@ -46,19 +27,6 @@ namespace AOC.NewFolder
                        .OrderByDescending(x => x)
                        .Take(3)
                        .Sum();
-        }
-
-
-        public static int SecondTaskNormalWay()
-        {
-            var input = File.ReadAllText(@"..\..\..\Day1\input1.txt");
-            var inputSplitted = input.Split("\r\n\r\n");
-            var energyTotals = new List<int>();
-
-            foreach (var values in inputSplitted)
-                energyTotals.Add(values.Split("\r\n").Select(x => Convert.ToInt32(x)).Sum());
-
-            return energyTotals.OrderByDescending(x=>x).Take(3).Sum();
         }
     }
 }
